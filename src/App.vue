effc6a6ff72b273ffe16c625fc6b1aa9e58e2841
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-12-11 15:02:00 +0700

    Update Layouts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/layout/Header.vue'
+import Menu from '@/components/layout/Menu.vue'
+import Content from '@/components/layout/Content.vue'
+import Footer from '@/components/layout/Footer.vue'
+
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Header />
+    <Menu />
+    <Content />
+    <Footer />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import * as vueRouter from "vue-router";
+import Home from "@/views/Home.vue";
 
 const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  }
 ];
 
 const router = vueRouter.createRouter({
