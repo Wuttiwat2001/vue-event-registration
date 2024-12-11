c3b6bdde3af789c9b8cdb164d46ceaf7cccbaad9
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-12-11 14:51:13 +0700

    Create vue project with vite

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import * as vueRouter from "vue-router";
+
+const routes = [
+];
+
+const router = vueRouter.createRouter({
+  history: vueRouter.createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+
+
+export default router;
