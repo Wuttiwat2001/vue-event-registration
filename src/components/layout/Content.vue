bc23ca21944831d770e83fa7272af74fa9aac679
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-12-12 11:19:37 +0700

    fix layout content

--- src/components/layout/Content.vue
+++ src/components/layout/Content.vue
@@ -2,7 +2,7 @@
 <script setup>
 </script>
 <template>
-  <a-layout-content class="tw-w-full tw-h-full  tw-p-6 tw-mx-6 tw-mt-4 tw-bg-white">
+  <a-layout-content class="tw-h-full  tw-p-6 tw-mx-6 tw-mt-4 tw-bg-white">
     <router-view></router-view>
   </a-layout-content>
 </template>
