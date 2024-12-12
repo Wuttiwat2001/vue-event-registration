e1978f3bd9b37e45bf2b2e7f16dda91f3d1c6b67
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-12-12 15:27:20 +0700

    create function restore login and logout

--- src/stores/useAuthStore.js
+++ src/stores/useAuthStore.js
@@ -3,7 +3,9 @@ import { defineStore } from "pinia";
 import { ref, reactive } from "vue";
 
 export const useAuthStore = defineStore("auth", () => {
-  const user = reactive({});
+  const user = reactive({
+    isLoggedIn: false,
+  });
   const fetchingStatus = ref("init");
 
   async function login(username, password) {
@@ -12,26 +14,55 @@ export const useAuthStore = defineStore("auth", () => {
       await new Promise((resolve) => setTimeout(resolve, 500));
       const response = await api.login(username, password);
       if (response.status === 200 && response.data.success) {
-        user = response.data.data;
+        user.isLoggedIn = true;
+        user = {
+          ...response.data.data,
+        };
         token = response.data.token;
         localStorage.setItem("token", JSON.stringify(token));
         localStorage.setItem("user", JSON.stringify(user));
         fetchingStatus.value = "success";
       } else {
         fetchingStatus.value = "failed";
-        user = {};
+        user = {
+          isLoggedIn: false,
+        };
       }
     } catch (error) {
       fetchingStatus.value = "failed";
-      user = {};
+      user = {
+        isLoggedIn: false,
+      };
     } finally {
       fetchingStatus.value = "success";
     }
+  }
+
+  function restoreLogin() {
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+    if (token && user) {
+      user = JSON.parse(user);
+    } else {
+      user = {
+        isLoggedIn: false,
+      };
+    }
+  }
+
+  function logout() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    user = {
+      isLoggedIn: false,
+    };
   }
 
   return {
     user,
     fetchingStatus,
     login,
+    restoreLogin,
+    logout
   };
 });
