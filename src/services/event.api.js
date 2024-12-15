3a1e18d0b3a27b1f402a128671b673a6e9bf8059
KhunCaptain <captenlnw_za@hotmail.com>
2024-12-16 01:58:51 +0700

    update api event

--- src/services/event.api.js
+++ src/services/event.api.js
@@ -1,8 +1,8 @@
 import httpClient from "@/services/httpClient";
 import { server } from "@/services/constants";
 
-const findAll = async () => {
-  return httpClient.get(`${server.EVENT_URL}`);
+const findAll = async (page,pageSize,search,startDate,endDate) => {
+  return httpClient.get(`${server.EVENT_URL}?page=${page}&pageSize=${pageSize}&search=${search}?startDate=${startDate}?endDate=${endDate}`);
 };
 
 export default {
