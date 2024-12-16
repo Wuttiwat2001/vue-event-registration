2531ccdf6aa2f3eaf4db02f19b29cd930bab3e98
KhunCaptain <captenlnw_za@hotmail.com>
2024-12-17 00:44:49 +0700

    hide footer when not login

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <script setup>
-import { ref,onMounted } from "vue";
+import { onMounted } from "vue";
 import Header from "@/components/layout/Header.vue";
 import Content from "@/components/layout/Content.vue";
-import Footer from '@/components/layout/Footer.vue';
+import Footer from "@/components/layout/Footer.vue";
 import { useAuthStore } from "@/stores/useAuthStore";
 
 const authStore = useAuthStore();
 
-
-
-
 onMounted(() => {
   authStore.restoreLogin();
 });
-
-
 </script>
 <template>
   <a-layout class="tw-min-h-screen">
     <a-layout class="tw-w-full tw-overflow-auto tw-flex tw-flex-col">
-      <Header v-if="authStore.user.isLoggedIn"/>
-      <Content/>
-      <Footer />
+      <Header v-if="authStore.user.isLoggedIn" />
+      <Content />
+      <Footer v-if="authStore.user.isLoggedIn" />
     </a-layout>
   </a-layout>
 </template>
