bf58e26d48843598a7dd36ee36935227f9a4cfc8
KhunCaptain <captenlnw_za@hotmail.com>
2024-12-17 00:44:59 +0700

    clear page not use

--- src/views/EventRegistration.vue
+++ src/views/EventRegistration.vue
@@ -237,194 +237,186 @@ onMounted(() => {
 });
 </script>
 <template>
-  <div v-if="$route.meta.showContent">
-    <a-typography-title class="tw-mb-1" :level="2"
-      >Event Registration</a-typography-title
-    >
-    <a-typography-text type="secondary"
-      >Below is a list of all events that are currently open for registration.
-      Click on the event name to proceed.</a-typography-text
-    >
-    <a-row>
-      <a-col class="tw-my-5 tw-p-3 tw-bg-[#f5f5f5]" :span="24">
-        <a-row :gutter="[16, 0]">
-          <a-col class="tw-my-3" :span="24" :md="8" :lg="8">
-            <p>Available Seats</p>
-            <a-select
-              @change="selectAvailableSeats"
-              v-model:value="availableSeats"
-              placeholder="Select number of seats"
-              class="tw-w-full"
-            >
-              <a-select-option value="any">All</a-select-option>
-              <a-select-option value="available">Available</a-select-option>
-              <a-select-option value="full">Full</a-select-option>
-            </a-select>
-          </a-col>
-          <a-col class="tw-my-3" :span="24" :md="8" :lg="8">
-            <p>Created At</p>
-            <a-range-picker
-              @change="selectCreatedAtDate"
-              v-model:value="createdAtDate"
-              class="tw-w-full"
-            />
-          </a-col>
-          <a-col class="tw-my-3" :span="24" :md="8" :lg="8">
-            <p>Updated At</p>
-            <a-range-picker
-              @change="selectUpdatedAtDate"
-              v-model:value="updatedAtDate"
-              class="tw-w-full"
-            />
-          </a-col>
-        </a-row>
+  <a-typography-title class="tw-mb-1" :level="2"
+    >Event Registration</a-typography-title
+  >
+  <a-typography-text type="secondary"
+    >Below is a list of all events that are currently open for registration.
+    Click on the event name to proceed.</a-typography-text
+  >
+  <a-row>
+    <a-col class="tw-my-5 tw-p-3 tw-bg-[#f5f5f5]" :span="24">
+      <a-row :gutter="[16, 0]">
+        <a-col class="tw-my-3" :span="24" :md="8" :lg="8">
+          <p>Available Seats</p>
+          <a-select
+            @change="selectAvailableSeats"
+            v-model:value="availableSeats"
+            placeholder="Select number of seats"
+            class="tw-w-full"
+          >
+            <a-select-option value="any">All</a-select-option>
+            <a-select-option value="available">Available</a-select-option>
+            <a-select-option value="full">Full</a-select-option>
+          </a-select>
+        </a-col>
+        <a-col class="tw-my-3" :span="24" :md="8" :lg="8">
+          <p>Created At</p>
+          <a-range-picker
+            @change="selectCreatedAtDate"
+            v-model:value="createdAtDate"
+            class="tw-w-full"
+          />
+        </a-col>
+        <a-col class="tw-my-3" :span="24" :md="8" :lg="8">
+          <p>Updated At</p>
+          <a-range-picker
+            @change="selectUpdatedAtDate"
+            v-model:value="updatedAtDate"
+            class="tw-w-full"
+          />
+        </a-col>
+      </a-row>
+      <a-row>
+        <a-col class="tw-flex tw-my-3" :span="24" :md="12" :lg="12">
+          <a-input
+            @keyup.enter="searchTable"
+            v-model:value="search"
+            placeholder="Search"
+          />
+          <a-button @click="searchTable" class="tw-mx-3" type="primary"
+            >GO</a-button
+          >
+        </a-col>
+      </a-row>
+    </a-col>
+  </a-row>
+
+  <a-row>
+    <a-col :span="24">
+      <a-card class="tw-rounded-none" :bodyStyle="{ padding: '0' }">
         <a-row>
-          <a-col class="tw-flex tw-my-3" :span="24" :md="12" :lg="12">
-            <a-input
-              @keyup.enter="searchTable"
-              v-model:value="search"
-              placeholder="Search"
-            />
-            <a-button @click="searchTable" class="tw-mx-3" type="primary"
-              >GO</a-button
+          <a-col :span="24" class="tw-p-4">
+            <a-row>
+              <a-col :span="24" class="tw-my-2 tw-flex tw-justify-end">
+                <a-select
+                  class="tw-mx-3 tw-w-[100px]"
+                  v-model:value="pageSize"
+                  :options="pageSizeOptions"
+                  @change="(value) => handleTableChange(value, 'pageSize')"
+                ></a-select>
+                <EventCreate @createEvent="handleCreateEvent" />
+              </a-col>
+            </a-row>
+          </a-col>
+          <a-divider class="tw-m-0" />
+          <a-col :span="24">
+            <a-table
+              :loading="eventStore.fetchingStatus === 'loading' ? true : false"
+              :columns="columns"
+              :data-source="eventStore.events"
+              :pagination="false"
+              :scroll="{
+                x: 'max-content',
+                y: '50vh',
+              }"
             >
+              <template #bodyCell="{ column, record }">
+                <template v-if="column.dataIndex === 'title'">
+                  <div class="tw-w-[280px] tw-truncate">
+                    <a-typography-text
+                      @click="onClickSearchItem(record.title)"
+                      class="trigger_text"
+                      strong
+                      >{{ record.title }}</a-typography-text
+                    >
+                    <br />
+                    <a-typography-text
+                      @click="onClickSearchItem(record.description)"
+                      class="trigger_text"
+                      type="secondary"
+                      >{{ record.description }}</a-typography-text
+                    >
+                  </div>
+                </template>
+                <template v-if="column.dataIndex === 'location'">
+                  <div class="tw-w-[200px] tw-truncate">
+                    <a-typography-text
+                      @click="onClickSearchItem(record.location)"
+                      class="trigger_text"
+                      strong
+                      >{{ record.location }}</a-typography-text
+                    >
+                  </div>
+                </template>
+
+                <template v-else-if="column.dataIndex === 'status'">
+                  <a-tag v-if="record.remainingSeats > 0" color="green"
+                    >Available</a-tag
+                  >
+                  <a-tag v-else color="red">Full</a-tag>
+                </template>
+
+                <template v-else-if="column.dataIndex === 'totalSeats'">
+                  <a-typography-text>{{
+                    filters.formatNumber(record.totalSeats)
+                  }}</a-typography-text>
+                </template>
+
+                <template v-else-if="column.dataIndex === 'remainingSeats'">
+                  <a-typography-text>{{
+                    filters.formatNumber(record.remainingSeats)
+                  }}</a-typography-text>
+                </template>
+
+                <template v-else-if="column.dataIndex === 'registeredUsers'">
+                  <a-typography-text strong>{{
+                    filters.formatNumber(record.registeredUsers.length)
+                  }}</a-typography-text>
+                </template>
+
+                <template v-else-if="column.dataIndex === 'createdAt'">
+                  <a-typography-text type="secondary">{{
+                    filters.formatDate(record.createdAt)
+                  }}</a-typography-text>
+                </template>
+
+                <template v-else-if="column.dataIndex === 'updatedAt'">
+                  <a-typography-text type="secondary">{{
+                    filters.formatDate(record.updatedAt)
+                  }}</a-typography-text>
+                </template>
+
+                <template v-else-if="column.dataIndex === 'action'">
+                  <div class="tw-flex tw-justify-center">
+                    <EventEdit @editEvent="handleEditEvent" :id="record._id" />
+                    <delete-outlined
+                      class="trigger_icon tw-ms-2 tw-text-red-500"
+                    />
+                  </div>
+                </template>
+              </template>
+            </a-table>
+          </a-col>
+          <a-col
+            :span="24"
+            class="tw-p-4 tw-flex tw-justify-between tw-items-center"
+          >
+            <a-typography-text type="secondary" strong>
+              Showing items {{ startItem }} to {{ endItem }}
+            </a-typography-text>
+            <a-pagination
+              v-model:current="currentPage"
+              :total="eventStore.totalEvents"
+              :show-size-changer="false"
+              :pageSize="pageSize"
+              @change="(value) => handleTableChange(value, 'pagination')"
+            />
           </a-col>
         </a-row>
-      </a-col>
-    </a-row>
-
-    <a-row>
-      <a-col :span="24">
-        <a-card class="tw-rounded-none" :bodyStyle="{ padding: '0' }">
-          <a-row>
-            <a-col :span="24" class="tw-p-4">
-              <a-row>
-                <a-col :span="24" class="tw-my-2 tw-flex tw-justify-end">
-                  <a-select
-                    class="tw-mx-3 tw-w-[100px]"
-                    v-model:value="pageSize"
-                    :options="pageSizeOptions"
-                    @change="(value) => handleTableChange(value, 'pageSize')"
-                  ></a-select>
-                  <EventCreate @createEvent="handleCreateEvent" />
-                </a-col>
-              </a-row>
-            </a-col>
-            <a-divider class="tw-m-0" />
-            <a-col :span="24">
-              <a-table
-                :loading="
-                  eventStore.fetchingStatus === 'loading' ? true : false
-                "
-                :columns="columns"
-                :data-source="eventStore.events"
-                :pagination="false"
-                :scroll="{
-                  x: 'max-content',
-                  y: '50vh',
-                }"
-              >
-                <template #bodyCell="{ column, record }">
-                  <template v-if="column.dataIndex === 'title'">
-                    <div class="tw-w-[280px] tw-truncate">
-                      <a-typography-text
-                        @click="onClickSearchItem(record.title)"
-                        class="trigger_text"
-                        strong
-                        >{{ record.title }}</a-typography-text
-                      >
-                      <br />
-                      <a-typography-text
-                        @click="onClickSearchItem(record.description)"
-                        class="trigger_text"
-                        type="secondary"
-                        >{{ record.description }}</a-typography-text
-                      >
-                    </div>
-                  </template>
-                  <template v-if="column.dataIndex === 'location'">
-                    <div class="tw-w-[200px] tw-truncate">
-                      <a-typography-text
-                        @click="onClickSearchItem(record.location)"
-                        class="trigger_text"
-                        strong
-                        >{{ record.location }}</a-typography-text
-                      >
-                    </div>
-                  </template>
-
-                  <template v-else-if="column.dataIndex === 'status'">
-                    <a-tag v-if="record.remainingSeats > 0" color="green"
-                      >Available</a-tag
-                    >
-                    <a-tag v-else color="red">Full</a-tag>
-                  </template>
-
-                  <template v-else-if="column.dataIndex === 'totalSeats'">
-                    <a-typography-text>{{
-                      filters.formatNumber(record.totalSeats)
-                    }}</a-typography-text>
-                  </template>
-
-                  <template v-else-if="column.dataIndex === 'remainingSeats'">
-                    <a-typography-text>{{
-                      filters.formatNumber(record.remainingSeats)
-                    }}</a-typography-text>
-                  </template>
-
-                  <template v-else-if="column.dataIndex === 'registeredUsers'">
-                    <a-typography-text strong>{{
-                      filters.formatNumber(record.registeredUsers.length)
-                    }}</a-typography-text>
-                  </template>
-
-                  <template v-else-if="column.dataIndex === 'createdAt'">
-                    <a-typography-text type="secondary">{{
-                      filters.formatDate(record.createdAt)
-                    }}</a-typography-text>
-                  </template>
-
-                  <template v-else-if="column.dataIndex === 'updatedAt'">
-                    <a-typography-text type="secondary">{{
-                      filters.formatDate(record.updatedAt)
-                    }}</a-typography-text>
-                  </template>
-
-                  <template v-else-if="column.dataIndex === 'action'">
-                    <div class="tw-flex tw-justify-center">
-                      <EventEdit
-                        @editEvent="handleEditEvent"
-                        :id="record._id"
-                      />
-                      <delete-outlined
-                        class="trigger_icon tw-ms-2 tw-text-red-500"
-                      />
-                    </div>
-                  </template>
-                </template>
-              </a-table>
-            </a-col>
-            <a-col
-              :span="24"
-              class="tw-p-4 tw-flex tw-justify-between tw-items-center"
-            >
-              <a-typography-text type="secondary" strong>
-                Showing items {{ startItem }} to {{ endItem }}
-              </a-typography-text>
-              <a-pagination
-                v-model:current="currentPage"
-                :total="eventStore.totalEvents"
-                :show-size-changer="false"
-                :pageSize="pageSize"
-                @change="(value) => handleTableChange(value, 'pagination')"
-              />
-            </a-col>
-          </a-row>
-        </a-card>
-      </a-col>
-    </a-row>
-  </div>
-  <router-view v-else></router-view>
+      </a-card>
+    </a-col>
+  </a-row>
 </template>
 
 <style scoped>
