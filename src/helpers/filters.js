182d5809ecabeab2780708dd665af436424774bd
KhunCaptain <captenlnw_za@hotmail.com>
2024-12-16 15:03:03 +0700

    format number

--- src/helpers/filters.js
+++ src/helpers/filters.js
@@ -1,6 +1,9 @@
 import dayjs from "dayjs";
 
 export default {
+  formatNumber(value) {
+    return new Intl.NumberFormat("en-IN").format(value);
+  },
   formatDate(value) {
     const date = dayjs(value);
     return date.format(`DD/MM/YYYY • HH:mm`);
